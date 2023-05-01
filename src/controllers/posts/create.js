728bc649e60e37285e972f1ca14cda963e7f6a8c
luis <lrivera2022@amsbronx.org>
2023-05-01 15:50:11 -0400

    filled post routes and model

---
+++ src/controllers/posts/create.js
@@ -0,0 +1,14 @@
+const createPost = async (req, res) => {
+    const {
+      session,
+      db: { Posts },
+      body: { user_id, post_text },
+    } = req;
+  
+    const post = await Posts.createPost(user_id, post_text);
+    session.userId = user.id;
+  
+    res.send(post);
+  };
+  
+  module.exports = createPost;
---
+++ src/controllers/posts/index.js
@@ -0,0 +1,9 @@
+const create = require('./create')
+const deletepost = require('./delete')
+const list = require('./list')
+
+module.exports = {
+    create,
+    deletepost,
+    list
+}
