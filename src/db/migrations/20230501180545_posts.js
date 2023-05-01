1168abffe414f5f6fd19f4152be70baaf886f9a2
oshaunberridge <oshaun1250@gmail.com>
2023-05-01 14:21:00 -0400

    finished making migrations and seeds

---
+++ src/db/migrations/20230501180545_posts.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+    return knex.schema.createTable('posts', (table) => {
+        table.increments('id');
+        table.integer('user_id').references('id').inTable('users');
+        table.string('post_text');
+        table.timestamps(true,true);
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('posts');
+};
