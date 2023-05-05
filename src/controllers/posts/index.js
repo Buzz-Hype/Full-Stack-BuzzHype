2c0026cb7137d8ffd0394c629e624a06ebf55b3a
luis <lrivera2022@amsbronx.org>
2023-05-05 19:43:02 -0400

    added update post route

--- src/controllers/posts/index.js
+++ src/controllers/posts/index.js
@@ -2,10 +2,12 @@ const create = require('./create')
 const deletepost = require('./delete')
 const list = require('./list')
 const find = require('./find')
+const update = require('./update')
 
 module.exports = {
     create,
     deletepost,
     list,
-    find
+    find,
+    update
 }
