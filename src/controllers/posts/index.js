9af14d7e3e3103506852e15ea75d56ba5184e716
luis <lrivera2022@amsbronx.org>
2023-05-05 11:59:22 -0400

    saving logic

--- src/controllers/posts/index.js
+++ src/controllers/posts/index.js
@@ -1,9 +1,11 @@
 const create = require('./create')
 const deletepost = require('./delete')
 const list = require('./list')
+const find = require('./find')
 
 module.exports = {
     create,
     deletepost,
-    list
+    list,
+    find
 }
